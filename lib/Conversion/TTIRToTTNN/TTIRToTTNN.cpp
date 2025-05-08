05936bedb552f91d95ae92e2a525ca3842da26f9
Hongseok Kwon <hkwon@tenstorrent.com>
2025-05-08 21:27:55 +0000

    basic TTNN work

--- lib/Conversion/TTIRToTTNN/TTIRToTTNN.cpp
+++ lib/Conversion/TTIRToTTNN/TTIRToTTNN.cpp
@@ -1403,7 +1403,8 @@ public:
     rewriter.replaceOpWithNewOp<ttnn::AllToAllOp>(
         op, this->getTypeConverter()->convertType(op.getType()),
         adaptor.getInput(), device, adaptor.getSplitDim(),
-        adaptor.getConcatDim(), adaptor.getClusterAxis());
+        adaptor.getConcatDim(), adaptor.getSplitCount(),
+        adaptor.getClusterAxis());
 
     return success();
   }
--- lib/Target/TTNN/TTNNToFlatbuffer.cpp
+++ lib/Target/TTNN/TTNNToFlatbuffer.cpp
@@ -698,6 +698,19 @@ createOp(FlatbufferObjectCache &cache, CollectivePermuteOp op) {
       cache.fbb->CreateVector<int64_t>(sourceTargetPairsVec));
 }
 
+::flatbuffers::Offset<::tt::target::ttnn::AllToAllOp>
+createOp(FlatbufferObjectCache &cache, AllToAllOp op) {
+  auto input = cache.at<::tt::target::ttnn::TensorRef>(
+      getOperandThroughDPSOps(op.getInput()));
+  auto output = cache.getOrCreate(op.getResult(), tensorValueToFlatbuffer,
+                                  kHostAllocatedSize);
+  auto device = getOperandThroughDPSOps(op.getDevice());
+  return ::tt::target::ttnn::CreateAllToAllOp(
+      *cache.fbb, input, output, cache.at<::tt::target::DeviceRef>(device),
+      op.getSplitDim(), op.getConcatDim(), op.getSplitCount(),
+      op.getClusterAxis());
+}
+
 ::flatbuffers::Offset<::tt::target::ttnn::MeshShardOp>
 createOp(FlatbufferObjectCache &cache, MeshShardOp op) {
   auto input = cache.at<::tt::target::ttnn::TensorRef>(
@@ -1995,6 +2008,10 @@ emitTTNNOperation(FlatbufferObjectCache &cache, Operation *op,
       collectivePermuteOp) {
     return createOperation(cache, createOp(cache, collectivePermuteOp),
                            debugString, locInfo);
+  }
+  if (auto allToAllOp = dyn_cast<AllToAllOp>(op); allToAllOp) {
+    return createOperation(cache, createOp(cache, allToAllOp), debugString,
+                           locInfo);
   }
   if (auto meshShardOp = dyn_cast<MeshShardOp>(op); meshShardOp) {
     return createOperation(cache, createOp(cache, meshShardOp), debugString,
