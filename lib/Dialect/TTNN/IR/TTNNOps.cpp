86696fc39230f25eabb103b6838431d4572ada04
Hongseok Kwon <hkwon@tenstorrent.com>
2025-05-22 21:00:34 +0000

    Basic TTNN Op definition

--- lib/Dialect/TTNN/IR/TTNNOps.cpp
+++ lib/Dialect/TTNN/IR/TTNNOps.cpp
@@ -2222,6 +2222,16 @@ mlir::tt::ttnn::CollectivePermuteOp::fold(FoldAdaptor adaptor) {
 }
 
 //===----------------------------------------------------------------------===//
+// PointToPointOp
+//===----------------------------------------------------------------------===//
+
+// PointToPointOp verification
+::mlir::LogicalResult mlir::tt::ttnn::PointToPointOp::verify() {
+  // ToDo: Verifier
+  return success();
+}
+
+//===----------------------------------------------------------------------===//
 // UpdateCacheOp
 //===----------------------------------------------------------------------===//
 
