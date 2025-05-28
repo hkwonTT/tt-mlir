9e1438e0e4f60e7a63c5e36cb3cf799f6a2b164e
Hongseok Kwon <hkwon@tenstorrent.com>
2025-05-28 21:18:50 +0000

    fix missing for AggregateAsTensor Op

--- lib/Target/TTNN/TTNNToFlatbuffer.cpp
+++ lib/Target/TTNN/TTNNToFlatbuffer.cpp
@@ -2154,6 +2154,11 @@ emitTTNNOperation(FlatbufferObjectCache &cache, Operation *op,
     return createOperation(cache, createOp(cache, pointToPointOp), debugString,
                            locInfo);
   }
+  if (auto aggregateAsTensorOp = dyn_cast<AggregateAsTensorOp>(op);
+      aggregateAsTensorOp) {
+    return createOperation(cache, createOp(cache, aggregateAsTensorOp),
+                           debugString, locInfo);
+  }
 
   llvm_unreachable("unhandled op in emitTTNNOperation");
 }
