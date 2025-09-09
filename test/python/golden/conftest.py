836695f724e4a118ac2e1af846ae25d53e0d199c
Hongseok Kwon <hkwon@tenstorrent.com>
2025-09-09 19:56:12 +0000

    multidevice-only

--- test/python/golden/conftest.py
+++ test/python/golden/conftest.py
@@ -174,6 +174,11 @@ def pytest_addoption(parser):
         action="store_true",
         help="disable putting dispatch on ethernet cores - place it on worker cores instead; necessary on blackhole",
     )
+    parser.addoption(
+        "--multidevice-only",
+        action="store_true",
+        help="Only run tests that require multidevice - temporary",
+    )
 
 
 def get_board_id(system_desc) -> str:
@@ -486,6 +491,9 @@ def pytest_collection_modifyitems(config, items):
                 system_desc, params, require_exact_mesh=config.option.require_exact_mesh
             ):
                 # Deselect the test case
+                deselected.append(item)
+                continue
+            if config.option.multidevice_only and not params.get("mesh_shape", False):
                 deselected.append(item)
                 continue
         valid_items.append(item)
